a45c1b7950b2db5206266e5665fc0eebda4f33e6
francois nicaise <whitetigle@gmail.com>
2019-08-07 18:09:50 +0200

    added features

--- src/components/HomePage.fs
+++ src/components/HomePage.fs
@@ -28,6 +28,16 @@ let whereToText: string =
   sprintf "[Try Fable online](%s), [check the docs](%s) or visit [fable-awesome](https://github.com/kunjee17/awesome-fable) for a curated list of Fable resources, join the community at [FableConf](%s) or watch the [Channel9 interview with Seth Juarez](https://channel9.msdn.com/events/NDC/NDC-Oslo-2017/C9L13?term=fable)."
     Navbar.Repl Navbar.Docs Navbar.FableConf
 
+let featuresText = 
+  [
+    "The Fable APIs have been written to avoid any situation where you would have to test for a null value. The F# language gives us the ability to wrap ``null`` into a nice ``Option``"      
+    "Express your code in an elegant an powerful fashion using computation expressions "
+    "Let the compiler verify arithmetic relationships for you to help prevent programming errors."
+    "Use compiler directives to change the behavior of your program."
+    "Type your data with Discriminated Unions to allow for better Domain understanding and compiler safety."
+    "An area where F# shines is its powerful pattern matching, ready to match any domain model!"
+  ]
+
 let linkImage src href =
   div [
     Style [
@@ -60,9 +70,35 @@ let prepareCode snippet =
           Style [
             BackgroundColor "whitesmoke"
             FontFamily "Fira Code"
-            FontSize "1rem"
+            FontSize "0.8rem"
           ] ] 
         [ str snippet ] ]
+
+module Features = 
+
+  let prepare title text snippet = 
+        Columns.columns [ 
+          Columns.IsVCentered
+          Columns.Props [
+            Style [
+              MarginTop "1.5rem"
+            ]
+          ]
+        ] [
+          Column.column [ Column.Width (Screen.All, Column.Is1) ] []
+          Column.column [ Column.Width (Screen.All, Column.Is3)] [
+            Heading.h4 [ Heading.Props [Style [ Color "dodgerblue" ] ] ] [ str title]
+            Content.content [] [
+              p [] [
+                parseMarkdownAsReactEl "" text
+              ]
+            ]
+          ]
+          Column.column [ Column.Width (Screen.All, Column.Is7)] [
+            (prepareCode snippet)
+          ]
+          Column.column [] []
+        ]
 
 let actionButtons = 
   Columns.columns 
@@ -185,6 +221,99 @@ npm install
 npm start"""
             ] ]
 
+      Section.section [
+        Section.Modifiers [
+          Modifier.BackgroundColor IsWhite
+        ]
+      ] [
+        Heading.h2 [ 
+          Heading.Props [Style [ Color "dodgerblue" ] ]
+          Heading.Modifiers [Modifier.TextAlignment (Screen.All, TextAlignment.Centered)]
+          ] 
+          [ str "Features"]
+        hr []
+        (Features.prepare 
+          "The end of null?"
+          featuresText.[0]
+"""
+// Is geolocation available for our navigator?
+let geolocation = Browser.navigator.geolocation
+
+match geolocation with 
+| Some geolocation -> JS.console.log "Geolocation enabled 😊"
+| None -> JS.console.log "Geolocation not enabled 😥"
+"""     )
+
+        (Features.prepare 
+          "Master of Monads!"
+          featuresText.[1]
+"""
+// Fetch & promises made easy with computation expressions
+promise {
+    let! res = fetch url []
+    let! txt = res.text()
+    return txt.Length
+}
+"""     )
+
+        (Features.prepare 
+          "Fireproof arithmetics"
+          featuresText.[2]
+"""
+[<Measure>] type cm
+let length = 12.0<cm>
+
+[<Measure>] type kg
+let weight = 9.2<kg>
+
+let thisWillFail = length + weight 
+// ERROR: The unit of measure 'kg' does 
+// not match the unit of measure 'cm'
+"""     )
+
+        (Features.prepare 
+          "Conditional compilation"
+          featuresText.[3]
+"""
+#if VERSION1
+let addition x y = 
+  x + y + 100
+
+#else
+let addition x y = 
+  x + y
+
+#endif
+
+let result = addition 1 1
+"""     )
+
+        (Features.prepare 
+          "Discriminated Unions"
+          featuresText.[4]
+"""
+type Face = Ace | King | Queen | Jack | Number of int
+type Color = Spades | Hearts | Diamonds | Clubs 
+type Card = Face * Color
+
+let aceOfHearts = Ace,Hearts
+let tenOfSpades = (Number 10), Spades
+"""     )
+
+        (Features.prepare 
+          "Powerful pattern matching"
+          featuresText.[5]
+"""
+match card with 
+| Hearts,Ace -> JS.console.log "Ace Of Hearts!"
+| _,Ace -> JS.console.log "Ace of any color"
+| Spades,(Number 10) -> JS.console.log "10 of Spades"
+| Spades,_ -> JS.console.log "any card of Spades"
+| _ -> JS.console.log  "any other card"
+"""     )
+
+      ]
+
       Section.section 
           [] 
           [ Heading.h4 
@@ -209,5 +338,5 @@ npm start"""
               linkImage "umc.png" "https://www.who-umc.org/"
             ]
         ] 
-      ]
     ]
+  ]
